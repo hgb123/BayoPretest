d8fd34e650b12262ac1cda6dbb202b36fa879494
Bao Ho <pippi.blockhead@gmail.com>
2016-05-28 12:24:50 +0700

    Phase 2: Solve the problem

---
+++ solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h> // Use bits/stdc++.h lib, MX constant for short
+#define MX 500000
+using namespace std;
+int main() {
+	
+	freopen("inp.txt","r",stdin);	// Use inp.txt as standard input stream, 
+	freopen("out.txt","w",stdout); 	// Prepare output file
+	FILE *tempFile = fopen("temp.txt","w");	// Create temp file for splitted data
+	long long int temp;
+	vector<long int> a;	// A container variable for array of integer
+	unsigned int t = MX;
+
+	return 0;	
+}
+
+
+
