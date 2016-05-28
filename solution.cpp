e0cb79715fb423029aa4cac106ae260130c2a8b9
Bao Ho <pippi.blockhead@gmail.com>
2016-05-28 12:28:16 +0700

    Read half of the input data and sort it

--- solution.cpp
+++ solution.cpp
@@ -2,13 +2,22 @@
 #define MX 500000
 using namespace std;
 int main() {
-	
+
+	/* Init */
 	freopen("inp.txt","r",stdin);	// Use inp.txt as standard input stream, 
 	freopen("out.txt","w",stdout); 	// Prepare output file
 	FILE *tempFile = fopen("temp.txt","w");	// Create temp file for splitted data
 	long long int temp;
 	vector<long int> a;	// A container variable for array of integer
 	unsigned int t = MX;
+	/* end of init */
+
+	/* Step 1: Read half of the input data, 2MB */
+	while(t--) {
+		cin>>temp;
+		a.push_back(temp);
+	}
+	sort(a.begin(),a.begin()+a.size()); // Sort it using function in STL algorithm.h
 
 	return 0;	
 }
